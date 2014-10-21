fe6640c93aeff830495aef460a77a906d6e1896f
IljaGoushcha <iljagou@yahoo.com>
2014-10-20 20:48:27 -0400

    fixed bug in MeetUp generate grou_ids

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,20 @@ class HomeController < ApplicationController
     @city = "no city sellected"
   end
 
-  def get_meetups
+  def generate_categories
+
     city = params["city"].gsub(" ","+")
     state = params["state"]
-    url_string = "https://api.meetup.com/2/open_events?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&state=#{state}&city=#{city}&country=US&page=20"
-    @meetups = HTTParty.get(url_string)
-  end
+    url_string = "https://api.meetup.com/2/open_events?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&state=#{state}&city=#{city}&country=US&page=100"
+    meetups = HTTParty.get(url_string)["results"]
+    @group_ids = meetups.map do |meetup|
+      meetup["group"]["id"]
+    end
+    # WORKS UP TO HERE
 
+    # @categories = group_ids.map do |group_id|
+    #   HTTParty.get("https://api.meetup.com/2/groups?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&group_id=#{group_id}&page=5")["results"]
+    # end
+    # HTTParty.get(C
+  end
 end
