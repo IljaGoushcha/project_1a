2b5d57212427f0b94b928df420e03ff0a825c1cf
IljaGoushcha <iljagou@yahoo.com>
2014-10-21 11:01:37 -0400

    search through 15 categories, added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,24 +4,46 @@ class HomeController < ApplicationController
     @city = "no city sellected"
   end
 
-  def generate_categories
-
+  def get_meetups
     city = params["city"].gsub(" ","+")
     state = params["state"]
-    meetups = HTTParty.get("https://api.meetup.com/2/open_events?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&state=#{state}&city=#{city}&country=US&page=50")["results"]
-    @group_ids = meetups.map do |meetup|
-      meetup["group"]["id"]
+
+    @categories = {
+      1 => "Arts & Cluture", 3 => "Cars & Motorcycle", 5 => "Dancing", 6 => "Education & Learning",
+      8 => "Fashion/Beauty", 9 => "Fitness", 10 => "Food & Drink", 11 => "Games", 17 => "Lifestyle",
+      18 => "Literature & Writing", 20 => "Movies & Film", 21 => "Music", 23 => "Outdoors & Adventure",
+      26 => "Pets/Animals", 27 => "Photography"
+    }
+
+    @results = Array.new
+    @categories.each do |key, value|
+      @results << HTTParty.get("https://api.meetup.com/2/open_events?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&state=#{state}&city=#{city}&country=US&category=#{key.to_i}&page=50")["meta"]["count"]
     end
+
+  end
+
+
+
+    # retiring this request
+    # meetups = HTTParty.get("https://api.meetup.com/2/open_events?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&state=#{state}&city=#{city}&country=US&page=5 0")["results"]
+    # @group_ids = meetups.map do |meetup|
+    #   meetup["group"]["id"]
+    # end
+
     # WORKS UP TO HERE
-    @first_group_id = @group_ids[0]
-    @category = HTTParty.get("https://api.meetup.com/2/groups?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&group_id=#{"@first_group_id"}&page=20")
-    # binding.pry
+    # @first_group_id = @group_ids[0]
+    # @group = HTTParty.get("https://api.meetup.com/2/groups?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&group_id=#{@first_group_id}&page=20")
+    # @category = @group["results"][0]["category"]
+
+    # Retired due to request limits at MeetUp
     # @categories = @group_ids.map do |group_id|
-    #   group_id + 1000000000000
-    #   HTTParty.get("https://api.meetup.com/2/groups?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&group_id=#{"group_id"}&page=1")["results"][0]["category"]
+    #   HTTParty.get("https://api.meetup.com/2/groups?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&group_id=#{group_id}&page=20")["results"][0]["category"]
+    #   sleep 0.001
+    #   # HTTParty.get("https://api.meetup.com/2/groups?&key=#{ENV["MEETUP_API_KEY"]}&sign=true&photo-host=public&group_id=6589142    &page=20")["results"][0]["category"]
     # end
 
     # this should work
     # HTTParty.get("https://api.meetup.com/2/groups?&key=426c7a3d714f70521072414643787210&sign=true&photo-host=public&group_id=7183412&page=1")["results"][0]["category"]
-  end
+    # HTTParty.get("https://api.meetup.com/2/groups?&key=#{MEETUP_API_KEY}&sign=true&photo-host=public&group_id=6589142&page=20")["results"][0]["category"]
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  post '/' => "home#generate_categories"
+  post '/' => "home#get_meetups"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
